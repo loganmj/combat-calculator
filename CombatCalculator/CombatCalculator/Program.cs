98d029b05b781bb384fd721d5a2906034e43af5b
Michael <michaellogan7860@gmail.com>
2025-03-13 13:02:35 -0500

    Added code to retrieve number of dice from user.

--- CombatCalculator/CombatCalculator/Program.cs
+++ CombatCalculator/CombatCalculator/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace CombatCalculator
+{
+
+    /// <summary>
+    /// Combat calculator class.
+    /// </summary>
+    public class CombatCalculator
+    {
+        #region Private Methods
+
+        /// <summary>
+        /// Retrieves the number of dice to roll from the user.
+        /// </summary>
+        /// <returns></returns>
+        private static int GetNumberOfDiceFromUser()
+        {
+            Console.WriteLine("Enter number of dice to roll:");
+            return !int.TryParse(Console.ReadLine(), out int numberOfDice) ? 0 : numberOfDice;
+        }
+
+        /// <summary>
+        /// Simulates the rolling of the given number of dice.
+        /// </summary>
+        /// <param name="numberOfDice"></param>
+        private static void RollDice(int numberOfDice)
+        {
+            Console.WriteLine($"Rolling {numberOfDice} dice ...\n");
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Main method performs combat calculation and outputs percent liklihood of success.
+        /// </summary>
+        public static void Main()
+        {
+            while (true)
+            {
+                // Get data from user
+                var numberOfDice = GetNumberOfDiceFromUser();
+
+                // Roll dice
+                RollDice(numberOfDice);
+            }
+        }
+
+        #endregion
+    }
+}
